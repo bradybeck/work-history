a802d52bb1f2dc6837236e2fb690e6c45ad5857b
bradybeck <bradybeck@gmail.com>
2021-10-01 21:46:20 -0500

    tweaks

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const history = require('./pandanHistory.json');
 const fs = require('fs')
 const fileName = './history.csv';
 
-let daysToDuration = {}; 
+let daysToDuration = {};
 
+// create the csv file
 var appendToFile = (csvLine) => {
     fs.appendFile(fileName, csvLine, function (err) {
         if (err) return console.log(err);
@@ -11,23 +12,23 @@ var appendToFile = (csvLine) => {
 }
 
 function isWeekend(dayOfWeek) {
-    return dayOfWeek === 6 || dayOfWeek  === 0;
+    return dayOfWeek === 6 || dayOfWeek === 0;
 }
 
+// convert seconds to display for csv
 function secondsToHms(d) {
     d = Number(d);
     var h = Math.floor(d / 3600);
     var m = Math.floor(d % 3600 / 60);
-    var s = Math.floor(d % 3600 % 60);
 
-    var hDisplay = h > 0 ? h  : "";
-    var mDisplay = m > 0 ? m / 60  : "";
+    var hDisplay = h > 0 ? h : "";
+    var mDisplay = m > 0 ? m / 60 : "";
 
     if (mDisplay) {
         mDisplay = Math.round(mDisplay * 100) / 100
     }
 
-    return hDisplay + mDisplay; 
+    return hDisplay + mDisplay;
 }
 
 // Group the history entries by day
@@ -43,19 +44,17 @@ history.forEach(entry => {
 });
 
 try {
-  const data = fs.writeFileSync('./history.csv', "date, day\n")
-  for (const [key, value] of Object.entries(daysToDuration)) {
-    const finalTime = secondsToHms(value); 
-    if (finalTime > 8) {
-        appendToFile(`${key}, ${finalTime}\n`);
-    } else {
-        appendToFile(`${key}, ,${finalTime}\n`);
+    const data = fs.writeFileSync('./history.csv', "date, day\n")
+    for (const [key, value] of Object.entries(daysToDuration)) {
+        const finalTime = secondsToHms(value);
+        // want to track days where I miss the 8 hours.
+        if (finalTime > 8) {
+            appendToFile(`${key}, ${finalTime}\n`);
+        } else {
+            appendToFile(`${key}, ,${finalTime}\n`);
+        }
     }
 
-    
-
-  }
-
 } catch (err) {
-  console.error(err)
+    console.error(err)
 }
